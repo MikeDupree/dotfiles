7d339dfff5102fd9ceadba450856ae0bbdb9fad5
mdupree <mikerdupree@gmail.com>
2023-02-26 16:56:30 -0800

    Update tmux create_session

--- custom_bin/create_session.sh
+++ custom_bin/create_session.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -z "$1" ] 
 then
@@ -6,13 +6,31 @@ then
   exit 1
 fi
 
+if [ -z "$2" ] 
+then
+  echo "No workspace layout"
+fi
 session=$1
 window=${session}:0
 pane=${window}.0
 tmux new -s "$session" -d
-tmux split-window -t "$pane" -v
-tmux resize-pane -t "$pane" -D 10
-tmux send-keys -t "$pane" C-l 'cat /home/$USER/success.txt' C-m
+
+#
+# Workspace Configuration
+#
+echo "CMD $0  :: NAME $1 :: Workspace type :: $2"
+
+if [ $2 = "workspace" ]
+then
+  echo "Workspace Enabled"
+  tmux split-window -t "$pane" -v
+  tmux resize-pane -t "$pane" -D 10
+fi
+#tmux send-keys -t "$pane" C-l 'cat /home/$USER/success.txt' C-m
+
+#
+# Attach to the new session.
+#
 tmux select-pane -t "$pane"
 tmux select-window -t "$window"
 tmux attach-session -t "$session"
